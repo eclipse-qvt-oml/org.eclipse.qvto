f6f09d179036f95575093367ed418383b381828f
Ed Willink <ed@willink.me.uk>
2019-02-08 16:05:17 +0000

    Testing 3
    Change-Id: I824a887a9e93730853c5ab1765f9b8bbd0337c00

--- plugins/org.eclipse.m2m.qvt.oml.common/src/org/eclipse/m2m/internal/qvt/oml/common/MDAConstants.java
+++ plugins/org.eclipse.m2m.qvt.oml.common/src/org/eclipse/m2m/internal/qvt/oml/common/MDAConstants.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2007, 2018 Borland Software Corporation and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
- * 
+ *
  * Contributors:
  *     Borland Software Corporation - initial API and implementation
  *******************************************************************************/
@@ -18,12 +18,14 @@ public interface MDAConstants {
 	public static final String QVTO_FILE_EXTENSION = "qvto"; //$NON-NLS-1$
 
 	public static final String QVTO_FILE_EXTENSION_WITH_DOT = "." + QVTO_FILE_EXTENSION; //$NON-NLS-1$
-	
+
 	public static final String QVTO_LAUNCH_CONFIGURATION_NAME = "QVTO"; //$NON-NLS-1$
-    
+
 	public static final String QVTO_TRACEFILE_EXTENSION = "qvtotrace"; //$NON-NLS-1$
 
 	public static final String QVTO_TRACEFILE_EXTENSION_WITH_DOT = "." + QVTO_TRACEFILE_EXTENSION; //$NON-NLS-1$
 
-	
+	public static final String ANOTHER_ONE_FOR_TEST = "ANOTHER_ONE_FOR_TEST"; //$NON-NLS-1$
+
+
 }
