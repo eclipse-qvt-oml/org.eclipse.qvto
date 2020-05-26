c4d04d47a7c349f946608d36e723a42eb9b5af12
Christopher Gerking <christopher.gerking@upb.de>
2020-05-26 22:46:28 +0200

    [472482] Resolve blackboxes in the presence of non-blackbox classes

--- plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/blackbox/BlackboxProvider.java
+++ plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/blackbox/BlackboxProvider.java
@@ -103,7 +103,7 @@ public abstract class BlackboxProvider {
 	
 	public abstract void cleanup();
 	
-	private void handleBlackboxException(BlackboxException e, BlackboxUnitDescriptor descriptor) {
+	protected void handleBlackboxException(BlackboxException e, BlackboxUnitDescriptor descriptor) {
 		
 		Diagnostic diagnostic = e.getDiagnostic();
 		if(diagnostic != null) {
