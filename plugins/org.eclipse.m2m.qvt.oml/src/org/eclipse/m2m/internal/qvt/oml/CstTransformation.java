6e90b0168ee2846e90cb414c4dbd10710534c9e5
Christopher Gerking <christopher.gerking@kit.edu>
2024-01-08 17:02:02 +0100

    [582828] Convert line delimiters to Unix

--- plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/CstTransformation.java
+++ plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/CstTransformation.java
@@ -37,18 +37,18 @@ import org.eclipse.m2m.internal.qvt.oml.expressions.OperationalTransformation;
 import org.eclipse.m2m.qvt.oml.ExecutionDiagnostic;
 
 public class CstTransformation implements Transformation {
-	
+
 	private URI fURI;
 	private EPackage.Registry fPackageRegistry;
 	private CompiledUnit fCompiledUnit;
 	private ExecutionDiagnostic fLoadDiagnostic;
-	private OperationalTransformation fTransformation;	
+	private OperationalTransformation fTransformation;
 	private QVTOCompiler fCompiler;
-	
+
 	public CstTransformation(URI uri) {
 		this(uri, EPackage.Registry.INSTANCE);
 	}
-	
+
 	public CstTransformation(URI uri, EPackage.Registry packageRegistry) {
 		if (uri == null) {
 			throw new IllegalArgumentException("null transformation URI"); //$NON-NLS-1$
@@ -56,100 +56,93 @@ public class CstTransformation implements Transformation {
 		fURI = uri;
 		fPackageRegistry = packageRegistry == null ? EPackage.Registry.INSTANCE : packageRegistry;
 	}
-	
-	protected CompiledUnit getCompiledUnit(IProgressMonitor monitor) throws MdaException {			
-		if (ExeXMISerializer.COMPILED_XMI_FILE_EXTENSION.equals(fURI.fileExtension())) {		
+
+	protected CompiledUnit getCompiledUnit(IProgressMonitor monitor) throws MdaException {
+		if (ExeXMISerializer.COMPILED_XMI_FILE_EXTENSION.equals(fURI.fileExtension())) {
 			return new CompiledUnit(fURI, getCompiler().getResourceSet());
 		}
 
 		UnitProxy proxy = UnitResolverFactory.Registry.INSTANCE.getUnit(fURI);
 		if (proxy == null) {
 			fLoadDiagnostic = new ExecutionDiagnosticImpl(Diagnostic.ERROR,
-					ExecutionDiagnostic.TRANSFORMATION_LOAD_FAILED, NLS.bind(
-							Messages.UnitNotFoundError, fURI));
+					ExecutionDiagnostic.TRANSFORMATION_LOAD_FAILED, NLS.bind(Messages.UnitNotFoundError, fURI));
 			return null;
 		}
-		
+
 		QVTOCompiler compiler = getCompiler();
-		return compiler.compile(proxy, null, monitor);		
+		return compiler.compile(proxy, null, monitor);
 	}
-	
+
 	private QVTOCompiler getCompiler() {
 		if (fCompiler == null) {
 			fCompiler = createCompiler();
 		}
-		
+
 		return fCompiler;
 	}
-	
+
 	protected QVTOCompiler createCompiler() {
-		if(fPackageRegistry == null) {
+		if (fPackageRegistry == null) {
 			return new QVTOCompiler();
 		}
-		
+
 		return new QVTOCompiler(fPackageRegistry);
 	}
-	
+
 	private void doLoad(IProgressMonitor monitor) {
 		fLoadDiagnostic = ExecutionDiagnosticImpl.createOkInstance();
-		
+
 		try {
 			fCompiledUnit = getCompiledUnit(monitor);
 		} catch (MdaException e) {
 			fLoadDiagnostic = new ExecutionDiagnosticImpl(Diagnostic.ERROR,
-					ExecutionDiagnostic.TRANSFORMATION_LOAD_FAILED, NLS.bind(
-							Messages.FailedToCompileUnitError, fURI));
+					ExecutionDiagnostic.TRANSFORMATION_LOAD_FAILED, NLS.bind(Messages.FailedToCompileUnitError, fURI));
 
 			fLoadDiagnostic.merge(BasicDiagnostic.toDiagnostic(e.getStatus()));
 		}
-		
+
 		if (fCompiledUnit != null && isSuccess(fLoadDiagnostic)) {
-			
+
 			ExecutionDiagnostic compilationDiagnostic = createCompilationDiagnostic(fCompiledUnit);
-			
+
 			if (isSuccess(compilationDiagnostic)) {
 				fLoadDiagnostic.addAll(compilationDiagnostic);
-			}
-			else {
+			} else {
 				compilationDiagnostic.addAll(fLoadDiagnostic);
 				fLoadDiagnostic = compilationDiagnostic;
-				
+
 				return;
 			}
-						
+
 			fTransformation = doGetTransformation();
-			
+
 			if (fTransformation == null) {
-				ExecutionDiagnostic transformationDiagnostic = new ExecutionDiagnosticImpl(
-					Diagnostic.ERROR,
-					ExecutionDiagnostic.TRANSFORMATION_LOAD_FAILED, 
-					NLS.bind(Messages.NotTransformationInUnitError,
-					fURI)
-				);
+				ExecutionDiagnostic transformationDiagnostic = new ExecutionDiagnosticImpl(Diagnostic.ERROR,
+						ExecutionDiagnostic.TRANSFORMATION_LOAD_FAILED,
+						NLS.bind(Messages.NotTransformationInUnitError, fURI));
 				transformationDiagnostic.addAll(fLoadDiagnostic);
 				fLoadDiagnostic = transformationDiagnostic;
-				
+
 				return;
 			}
-			
+
 			ExecutionDiagnostic executabilityDiagnostic = checkIsExecutable(fTransformation);
-			
+
 			if (isSuccess(executabilityDiagnostic)) {
 				fLoadDiagnostic.addAll(executabilityDiagnostic);
-			}
-			else {
+			} else {
 				executabilityDiagnostic.addAll(fLoadDiagnostic);
 				fLoadDiagnostic = executabilityDiagnostic;
 			}
 		}
 	}
-	
+
 	private OperationalTransformation doGetTransformation() {
 		// TODO - cached the transformation selected as main
-		if(fCompiledUnit == null) {
+		if (fCompiledUnit == null) {
 			return null;
 		}
-		
+
 		List<Module> allModules = fCompiledUnit.getModules();
 		for (Module module : allModules) {
 			if (module instanceof OperationalTransformation) {
@@ -159,66 +152,59 @@ public class CstTransformation implements Transformation {
 
 		return null;
 	}
-	
-	private static ExecutionDiagnostic createCompilationDiagnostic(
-			CompiledUnit compiledUnit) {
-		
+
+	private static ExecutionDiagnostic createCompilationDiagnostic(CompiledUnit compiledUnit) {
+
 		ExecutionDiagnostic mainDiagnostic = ExecutionDiagnosticImpl.createOkInstance();
-				
+
 		if (!compiledUnit.getErrors().isEmpty()) {
-			
+
 			URI uri = compiledUnit.getURI();
-			
-			mainDiagnostic = new ExecutionDiagnosticImpl(
-					Diagnostic.ERROR, ExecutionDiagnostic.VALIDATION, NLS.bind(
-							Messages.CompilationErrorsFoundInUnit, uri.toString()));
-			
+
+			mainDiagnostic = new ExecutionDiagnosticImpl(Diagnostic.ERROR, ExecutionDiagnostic.VALIDATION,
+					NLS.bind(Messages.CompilationErrorsFoundInUnit, uri.toString()));
+
 			for (Diagnostic error : compiledUnit.getErrors()) {
 				mainDiagnostic.add(error);
 			}
 		}
-		
+
 		for (Diagnostic warning : compiledUnit.getWarnings()) {
 			mainDiagnostic.add(warning);
 		}
-		
+
 		return mainDiagnostic;
 	}
-		
+
 	public OperationalTransformation getTransformation(IProgressMonitor monitor) {
 		loadTransformation(monitor);
-		
+
 		return fTransformation;
 	}
-	
-	private static ExecutionDiagnostic checkIsExecutable(
-			OperationalTransformation transformation) {
-		
+
+	private static ExecutionDiagnostic checkIsExecutable(OperationalTransformation transformation) {
+
 		if (transformation.isIsBlackbox()) {
 			return ExecutionDiagnosticImpl.createOkInstance();
 		}
-		
+
 		EList<EOperation> operations = transformation.getEOperations();
 		for (EOperation oper : operations) {
-			if (oper instanceof ImperativeOperation
-					&& QvtOperationalEnv.MAIN.equals(oper.getName())) {
+			if (oper instanceof ImperativeOperation && QvtOperationalEnv.MAIN.equals(oper.getName())) {
 				return ExecutionDiagnosticImpl.createOkInstance();
 			}
 		}
 
-		return new ExecutionDiagnosticImpl(Diagnostic.ERROR,
-				ExecutionDiagnostic.VALIDATION, NLS.bind(
-						Messages.NoTransformationEntryPointError,
-						transformation.getName()));
+		return new ExecutionDiagnosticImpl(Diagnostic.ERROR, ExecutionDiagnostic.VALIDATION,
+				NLS.bind(Messages.NoTransformationEntryPointError, transformation.getName()));
 	}
-	
+
 	/**
-	 * Attempts to load the transformation and checks
-	 * if it is valid for execution.
+	 * Attempts to load the transformation and checks if it is valid for execution.
 	 * <p>
-	 * <b>Remark:</b></br> Only the first performs the actual transformation
-	 * loading, subsequent calls to this method will return the existing
-	 * diagnostic.
+	 * <b>Remark:</b></br>
+	 * Only the first performs the actual transformation loading, subsequent calls
+	 * to this method will return the existing diagnostic.
 	 * 
 	 * @return the diagnostic indicating possible problems of the load action
 	 */
@@ -228,36 +214,35 @@ public class CstTransformation implements Transformation {
 				doLoad(monitor);
 			}
 			return fLoadDiagnostic;
-		} 
-		finally {
+		} finally {
 			monitor.done();
 		}
 	}
-	
+
 	public ExecutionDiagnostic getDiagnostic() {
 		loadTransformation(new NullProgressMonitor());
-		
+
 		return fLoadDiagnostic;
 	}
-	
+
 	public URI getURI() {
 		return fURI;
 	}
-		
+
 	public ResourceSet getResourceSet() {
 		return getUnit().getResourceSet();
 	}
-	
+
 	public CompiledUnit getUnit() {
 		loadTransformation(new NullProgressMonitor());
 		return fCompiledUnit;
 	}
-	
+
 	public void cleanup() {
 		if (getResourceSet() != null) {
 			EmfUtil.cleanupResourceSet(getResourceSet());
 		}
-				
+
 		getCompiler().cleanup();
 	}
 
